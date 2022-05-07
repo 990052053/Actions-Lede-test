91e14a33d0a01fb425bd3096e01f358f15734d11
kakashi <77133451+990052053@users.noreply.github.com>
2022-05-07 08:01:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Add custom feeds source
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
  
